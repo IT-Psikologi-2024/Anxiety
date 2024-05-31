846f6ea934953b67f1fe116bff02a88669c6d75c
mhmmdmadhanip <muhammad.madhani@ui.ac.id>
2024-06-01 01:45:29 +0700

    Make new Navbar and NavItem Components

---
+++ src/app/components/NavItem.tsx
@@ -0,0 +1,30 @@
+'use client'
+import React from 'react';
+import { useRouter } from 'next/navigation';
+
+interface NavItemProps {
+  href: string;
+  children: React.ReactNode;
+}
+
+const NavItem: React.FC<NavItemProps> = ({ href, children }) => {
+  const router = useRouter();
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    router.push(href);
+  };
+
+  return (
+    <li>
+      <a 
+        href={href} 
+        onClick={handleClick} // Call handleClick function on click
+        className="px-2 md:px-3 py-1 md:py-2 rounded hover:font-semibold"
+      >
+        {children}
+      </a>
+    </li>
+  );
+};
+
+export default NavItem;
