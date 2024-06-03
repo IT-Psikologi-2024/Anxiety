46bb3d03a6c1d165340912ec50b2f18002106150
mhmmdmadhanip <muhammad.madhani@ui.ac.id>
2024-06-04 03:35:43 +0700

    Add: Background Ticket Page and Background Merch Page

--- src/app/tickettypes/page.tsx
+++ src/app/tickettypes/page.tsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import { Navbar } from '../components/Navbar'
+import Image from 'next/image'
+
 
 const TicketTypesPage = () => {
   return (
-    <>
-    <Navbar />
-    <div>
-        <h1>Ticket here!</h1>
+    <div className='absolute w-full h-[1117px]'>
+      <Navbar />
+      <Image 
+            src="/bg-ticket-1.svg"
+            fill
+            alt="Ticket Background"
+            sizes='none'
+            className='object-cover'
+          />
     </div>
-    </>
   )
 }
 
